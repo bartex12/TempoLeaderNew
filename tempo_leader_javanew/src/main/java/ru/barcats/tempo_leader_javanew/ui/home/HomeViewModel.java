f348315cd581854e2287920fbdd2142fdcb9a7a9
Андрей <barcats@inbox.ru>
2020-04-05 20:44:59 +0300

    переименовал модуль

---
+++ tempo_leader_javanew/src/main/java/ru/barcats/tempo_leader_javanew/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package ru.barcats.tempo_leader_javanew.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is mainMenu");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ tempo_leader_javanew/src/main/java/ru/barcats/tempo_leader_javanew/ui/share/ShareViewModel.java
@@ -0,0 +1,19 @@
+package ru.barcats.tempo_leader_javanew.ui.share;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ShareViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ShareViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is share fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
