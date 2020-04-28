03d6b759f29cf568b7f6895d04921b89476cdaae
Андрей <barcats@inbox.ru>
2020-04-28 19:20:28 +0300

    убрал database из timerTask - теперь этот класс без ссылок на базу данных

--- tempo_leader_javanew/src/main/java/ru/barcats/tempo_leader_javanew/ui/tempoleader/TempoleaderViewModel.java
+++ tempo_leader_javanew/src/main/java/ru/barcats/tempo_leader_javanew/ui/tempoleader/TempoleaderViewModel.java
@@ -8,7 +8,6 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 import ru.barcats.tempo_leader_javanew.model.DataSet;
 
 public class TempoleaderViewModel extends AndroidViewModel {
